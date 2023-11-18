570c8cb3b7d7a11ee2845652b107ed4f7e93e1a0
ina88 <ina88@DESKTOP-C06UQLK>
2023-11-18 11:20:46 +0900

    修改對照

---
+++ OnlineShopCMS/Areas/Identity/Data/OnlineShopCMSUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace OnlineShopCMS.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the OnlineShopCMSUser class
+public class OnlineShopCMSUser : IdentityUser
+{
+}
+
--- OnlineShopCMS/Program.cs
+++ OnlineShopCMS/Program.cs
@@ -1,14 +1,65 @@
-ï»¿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using OnlineShopCMS.Data;
+using Microsoft.AspNetCore.Identity;
+using OnlineShopCMS.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddDbContext<OnlineShopCMSContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("OnlineShopCMSContext") ?? throw new InvalidOperationException("Connection string 'OnlineShopCMSContext' not found.")));
+
+//builder.Services.AddDefaultIdentity<OnlineShopCMSUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<OnlineShopUserContextConnection>();
+
+//// Add services to the container.
+//builder.Services.AddControllersWithViews();
+
+//test
+var connectionString = builder.Configuration.GetConnectionString("OnlineShopCMSContext");
 builder.Services.AddDbContext<OnlineShopCMSContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("OnlineShopCMSContext") ?? throw new InvalidOperationException("Connection string 'OnlineShopCMSContext' not found.")));
+    options.UseSqlServer(connectionString));
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<OnlineShopCMSContext>();
+builder.Services.AddRazorPages();
 
-var app = builder.Build();
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    // Cookie settings
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+    options.LoginPath = "/Identity/Account/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+    options.SlidingExpiration = true;
+});
+
+//test end
+
+
+var app = builder.Build();//­n«ç»ò§PÂ_¿ù»~
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -22,11 +73,15 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
+app.UseAuthentication();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapRazorPages();
+});
